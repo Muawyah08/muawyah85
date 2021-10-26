ba9abe9ef7be4f463d5a95858d1032488884f3dd
LAPTOP-K9DLCFRB\ASUS ROG <60900119031@uin-alauddin.ac.id>
2021-10-26 12:02:28 -0700

    malam

---
+++ resources/views/alasan.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>muawyah_60900119031</title>
+    </head>
+<body>
+    <body style="background-color:rgb(219, 209, 174)"> </body>
+    <center>
+    <h1>Alasan Belajar Kakas</h1>
+    <p> Alasan saya belajar kakas untuk cepat skom</p>	
+</body>
+</html>
